d747b688282598ddb5398c1afd5f2b38ead443a8
Roberto Cunha <rcunha@live.com>
2024-05-14 17:07:22 -0300

    .

--- PassIn.Infrastructure/PassInDbContext.cs
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -12,8 +12,7 @@ namespace PassIn.Infrastructure
 
         protected override void OnConfiguring(
              DbContextOptionsBuilder optionsBuilder)
-               => optionsBuilder.UseSqlServer(connectionString: "Server = tcp:rcunha.database.windows.net, 1433; Initial Catalog = cunha_database; Persist Security Info=False;User ID = rcunha_db; Password=Rgf980711;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;",
-                   sqlServerOptions => sqlServerOptions.EnableRetryOnFailure());      
+               => optionsBuilder.UseSqlServer(connectionString: "Server = tcp:rcunha.database.windows.net, 1433; Initial Catalog = cunha_database; Persist Security Info=False;User ID = rcunha_db; Password=Rgf980711;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");      
    
     }
   }
